37ef3af572c6a2c7e667d70539223d8dbe746a14
Daniel Barrell <daniel.barrell@eaglegenomics.com>
2016-01-25 14:12:55 +0000

    License, maintainers, description etc

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,9 @@
 name 'TopHat'
-maintainer 'The Authors'
+maintainer 'Eagle Genomics'
 maintainer_email 'chef@eaglegenomics.com'
-license 'all_rights'
+license 'apache2'
 description 'Installs/Configures TopHat'
-long_description 'Installs/Configures TopHat'
-
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.1'
 
 depends 'build-essential'
